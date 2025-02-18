3a2fd019bf0a7a074c1b1373b65ff061a2260654
tkramer@relaytx.com <tkramer@relaytx.com>
2025-02-18 13:37:06 -0500

    lambda

--- src/api/models/bedrock.py
+++ src/api/models/bedrock.py
@@ -248,7 +248,7 @@ class BedrockModel(BaseChatModel):
                         return self._create_response(
                             model=chat_request.model,
                             message_id=message_id,
-                            content=[{"text": results["results"]}],
+                            content=[{"text": f'```{results["results"]}```'}],
                             finish_reason="stop"
                         )
                     else:
@@ -327,7 +327,7 @@ class BedrockModel(BaseChatModel):
                             choices=[
                                 ChoiceDelta(
                                     index=0,
-                                    delta=ChatResponseMessage(role="assistant", content=results["results"]),
+                                    delta=ChatResponseMessage(role="assistant", content=f'```{results["results"]}```'),
                                     logprobs=None,
                                     finish_reason="stop",
                                 )
